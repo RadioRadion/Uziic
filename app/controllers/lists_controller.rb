f9e89178fc88090a7a21abeb1edeb2668080a0c7
thibault rahou <trahou33@gmail.com>
2019-11-27 17:08:50 +0100

    indexplaylistsearch

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,17 @@
 class ListsController < ApplicationController
   def index
-    @lists = current_user.lists
+    if params[:query].present? || params[:query_town].present?
+      sql_query = " \
+      lists.name ILIKE :query \
+      OR lists.tags ILIKE :query \
+      "
+      sql_query_town = "\
+      users.address ILIKE :query_town \
+      "
+      @lists = List.where(sql_query, query: "%#{params[:query]}%").joins(:user).where(sql_query_town, query_town: "%#{params[:query_town]}%")
+    else
+      @lists = current_user.lists
+    end
     @list = List.new
   end
 
