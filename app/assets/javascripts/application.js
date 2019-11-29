daefb50c55ffbe0fda26cd13c6951fa7b454b9c8
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2019-11-29 14:47:57 +0100

    test heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-*= require plyr;
-
-document.addEventListener("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
-
-$(document).on("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
