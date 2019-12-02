4f3fb2e02b23d24fe8df5340f69c42f3d713b6aa
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2019-12-02 18:37:02 +0100

    player js en moins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,10 @@
 //= require rails-ujs
 //= require_tree
 
-document.addEventListener("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
+// document.addEventListener("turbolinks:load", function() {
+//   const player = new Plyr('#player');
+// });
 
-$(document).on("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
+// $(document).on("turbolinks:load", function() {
+//   const player = new Plyr('#player');
+// });
