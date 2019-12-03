2a45e521f187e604c1b459c4858df5dc42facbab
clement camara <clementcamara@gmail.com>
2019-12-03 12:37:46 +0100

    card song index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
 //= require rails-ujs
 //= require_tree
 
-document.addEventListener("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
-
-$(document).on("turbolinks:load", function() {
-  const player = new Plyr('#player');
-});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
 import "bootstrap";
 import "../plugins/flatpickr";
-import "../plugins/player";
+
 
