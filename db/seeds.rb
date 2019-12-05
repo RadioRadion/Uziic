e7a24cc9207f0bbb8436a37e8359322efff5b458
philippe CHIRIE <p.chirie@laposte.net>
2019-12-05 10:59:55 +0100

    UpdateSeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,86 +1,100 @@
 require 'faker'
 
-puts 'Creating 10 users...'
-10.times do
+puts 'Creating 16 users...'
+8.times do
   user = User.new(
     email: Faker::Internet.free_email,
     password: "azerty",
     name: Faker::FunnyName.two_word_name,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}"
+    pseudo: Faker::Artist.name,
+    address: "#{Faker::Address.street_address}, Bordeaux"
   )
   user.save!
+  2.times do
+    song = Song.new(
+      tags: "Rock",
+      title: Faker::Music.chord,
+      description: "Description",
+      post_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
+      duration: rand(60..5000)
+    )
+    song.user = user
+    song.save!
+  end
 end
-puts 'Finished!'
 
-U1=User.first
-U2=User.last
-
-puts 'Creating 50 songs...'
-25.times do
+8.times do
+  user = User.new(
+    email: Faker::Internet.free_email,
+    password: "azerty",
+    name: Faker::FunnyName.two_word_name,
+    pseudo: Faker::Artist.name,
+    address: "#{Faker::Address.street_address}, Toulouse"
+  )
+  user.save!
+  2.times do
   song = Song.new(
-    tags: Faker::Music.genre,
+    tags: "Electro",
     title: Faker::Music.chord,
     description: "Description",
     post_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
     duration: rand(60..5000),
-    user_id: User.first.id
   )
+  song.user = user
   song.save!
 end
-25.times do
-  song = Song.new(
-    tags: Faker::Music.genre,
-    title: Faker::Music.chord,
-    description: "Description",
-    post_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
-    duration: rand(60..5000),
-    user_id: User.last.id
-  )
-  song.save!
+
 end
-puts 'Finished!'
-puts 'Creating 10 lists'
-5.times do
+puts 'Finished users!'
+
+puts 'Creating 4 lists'
+2.times do
   list = List.new(
-    tags: Faker::Music.genre,
+    tags: "Rock",
     name: Faker::Music.album,
     post_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
     user_id: User.first.id
   )
   list.save!
 end
-5.times do
+2.times do
   list = List.new(
-    tags: Faker::Music.genre,
+    tags: "Electro",
     name: Faker::Music.album,
     post_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
-    user_id: User.last.id
+    user_id: User.second.id
   )
   list.save!
 end
-puts 'Finished!'
-puts 'Creating 2 play-list'
-  pl = ListSong.new(
-    list_id: List.first.id,
-    song_id: Song.first.id
-  )
-  pl.save!
-  pl = ListSong.new(
-    list_id: List.first.id,
-    song_id: Song.last.id
-  )
-  pl.save!
+puts 'Finished lists!'
+puts 'Completing 4 lists'
 
-  pl = ListSong.new(
-    list_id: List.last.id,
-    song_id: Song.first.id
-  )
-  pl.save!
-  pl = ListSong.new(
-    list_id: List.last.id,
-    song_id: Song.last.id
-  )
-  pl.save!
-puts 'Finished!'
-
-
+2.times do
+  listsong = ListSong.new(
+    list_id: 1,
+    song_id: rand(1..16)
+    )
+  listsong.save!
+end
+2.times do
+  listsong = ListSong.new(
+    list_id: 2,
+    song_id: rand(1..16)
+    )
+  listsong.save!
+end
+2.times do
+  listsong = ListSong.new(
+    list_id: 3,
+    song_id: rand(17..32)
+    )
+  listsong.save!
+end
+2.times do
+  listsong = ListSong.new(
+    list_id: 4,
+    song_id: rand(17..32)
+    )
+  listsong.save!
+end
+puts 'Finished lists!'
